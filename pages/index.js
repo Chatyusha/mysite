e13b34157a965dc1e38ce047ff353a4618da26db
Chatyusha <hirounicat76@gmail.com>
2022-07-13 20:18:00 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Nav from '../components/navigation'
+
 export default function Home() {
+  // return (
+  //   <h1 className="text-3xl font-bold underline">
+  //     Hello world!
+  //   </h1>
+  // )
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <dev>
+    <Nav>
+    </Nav>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </dev>
   )
 }
