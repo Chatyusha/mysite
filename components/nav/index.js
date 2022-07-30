8f7ae61fa64ad21ebefaa36853a0d88a437c2ff1
Chatyusha <hirounicat76@gmail.com>
2022-07-31 01:00:15 +0900

    update

--- components/nav/index.js
+++ components/nav/index.js
@@ -1,13 +1,80 @@
 import SimpleDrowdown from "../dropdown/simple"
 
+// var NavTree = {
+//     "プログラミング":[
+//         "Python",
+//         "Linux",
+//         "C++",
+//         "C#"
+//     ],
+//     "Unity":[
+//         "Rigidbody"
+//     ],
+//     "数学":[
+//         "三角関数"
+//     ],
+//     "その他技術":[
+//         "Raspberry Pi"
+//     ],
+//     "料理":[
+//         "マフィン"
+//     ]
+// }
+
+var NavTree = [
+    {
+        "category":"プログラミング",
+        "items":[
+            "Python",
+            "C#"
+        ]
+    },
+    {
+        "category":"Unity",
+        "items":[
+            "Rigidbody",
+        ]
+    }
+]
+var NavMenuJSX = NavTree.map(item => 
+    {
+        var Items = item.items.map(element => {return <li>{element}</li>})
+        return (
+            <dev>
+                <li>
+                    <a>{item.category}</a>
+                </li>
+                <div className={"uk-navbar-dropdown"}>
+                    <ul className={"uk-nav","uk-navbar-dropdown-nav"}>
+                        <li>{Items}</li>
+                    </ul>
+                </div>
+            </dev>
+        )
+    }
+)
+
 function NavMenu() {
     return (
         <div className={"uk-navbar-container"} uk-navbar="true">
             <div className={"uk-navbar-left"}>
                 <ul className={"uk-navbar-nav"}>
-                    <li className={"uk-active"}><a href="">hoge</a></li>
-                    <li className={"uk-parent"}><a href="">fuga</a></li>
+                    {/* <li>
+                        <a>hoge</a>
+                        <div className={"uk-navbar-dropdown"}>
+                            <ul className={"uk-nav","uk-navbar-dropdown-nav"}>
+                                <li class="uk-active"><a href="#">Active</a></li>
+                                <li><a href="#">Item</a></li>
+                                <li><a href="#">Item</a></li>
+
+                            </ul>
+                        </div>
+                        
+                    </li> */}
+                    {NavMenuJSX}
                 </ul>
+            </div>
+            <div>
             </div>
         </div>
     )
