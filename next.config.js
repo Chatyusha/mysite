bebb6b66c26422a503866fe72ee05c114673b630
BlackCat <44997646+Chatyusha@users.noreply.github.com>
2022-09-03 15:38:12 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  // some configuration
+  assetPrefix: process.env.NODE_ENV === "production" ? "/mysite" : ""
+  // another configuration
+};
+
